c55527baec381c0408ba2bdffaf86c2fe260db7d
Peter Ling <44208962+ling1984@users.noreply.github.com>
2026-03-18 20:57:25 +0000

    Can run the driver with args. Now need to do sliders

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,86 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use std::{process::{Child, Command}, sync::Mutex};
+
+use tauri::State;
+
+struct DriverState {
+    driver: Mutex<Option<Child>>,  // later we do drivers: Mutex<HashMap<u32, Child>>
 }
+
+#[tauri::command]
+fn start_racer(state: State<DriverState>) -> Result<(), String> {    
+    // Get the current driver process if it exists
+    let mut guard = state.driver.lock().unwrap();
+    if guard.is_some() {
+        return Err("Process already running".into());
+    }
+    // Run the process if it doesn't exist
+    // final racerunner.exe needs to be same dir as gym_torcs
+
+    // get the current exe path then go up one
+    let exe_dir = std::env::current_exe()
+        .expect("can't get exe path")
+        .parent()
+        .expect("exe has no parent")
+        .to_path_buf();
+    let driver_script_path = exe_dir.join("gym_torcs").join("torcs_jm_par.py");
+    println!("Running driver script at: {:?}", driver_script_path);
+    let child = Command::new("python")
+        .arg(&driver_script_path)
+        .spawn()
+        .map_err(|e| e.to_string())?;
+
+    *guard = Some(child);
+
+    Ok(())
+}
+
+#[tauri::command]
+fn stop_racer(state: State<DriverState>) -> Result<(), String> {
+    let mut guard = state.driver.lock().unwrap();
+
+    if let Some(child) = guard.as_mut() {
+        child.kill().map_err(|e| e.to_string())?;
+        child.wait().ok(); // cleanup
+
+        *guard = None;
+        Ok(())
+    } else {
+        Err("No process running".into())
+    }
+}
+
+// fn run_python_driver_script() -> Child {
+//     let port = format!("3001");
+//     // get the current exe path then go up one
+//     let exe_dir = std::env::current_exe()
+//         .expect("can't get exe path")
+//         .parent()
+//         .expect("exe has no parent")
+//         .to_path_buf();
+
+//     // final racerunner.exe needs to be same dir as gym_torcs
+//     let driver_script_path = exe_dir.join("gym_torcs").join("torcs_jm_par.py");
+
+//     Command::new("python")
+//         .arg(&driver_script_path)
+//         .arg("--port")
+//         .arg(port)
+//         .arg("--features")
+//         .arg(format!("{},30,0.20,0.9,0,20,40,80,100,180,true", 180))
+//         .spawn()
+//         .expect("failed to execute process")
+
+// }
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(DriverState {
+            driver: Mutex::new(None),
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![start_racer, stop_racer])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,37 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+    import { run } from "svelte/legacy";
 
-  let name = $state("");
-  let greetMsg = $state("");
+  let target_speed = $state("");
+  let msg = $state("Waiting...");
+  let running = false;
 
-  async function greet(event: Event) {
+  async function handle_run_clicked(event: Event) {
     event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
+    if (!running) {
+      msg = await invoke("start_racer");
+      msg = "Started";
+      running = true;
+    } 
+    else {
+      msg = await invoke("stop_racer");
+      msg = "Stopped";
+      running = false;
+    }
   }
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <h1>Start your racer</h1>
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
+  <form class="row" onsubmit={handle_run_clicked}>
+    <input id="target-input" placeholder="Enter a target speed..." bind:value={target_speed} />
+    <button type="submit">Run</button>
   </form>
-  <p>{greetMsg}</p>
+  <p>{msg}</p>
 </main>
 
 <style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
 
 :root {
   font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
@@ -68,17 +58,6 @@
   text-align: center;
 }
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
 .row {
   display: flex;
   justify-content: center;
@@ -88,10 +67,6 @@ a {
   font-weight: 500;
   color: #646cff;
   text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
 }
 
 h1 {
@@ -129,7 +104,7 @@ button {
   outline: none;
 }
 
-#greet-input {
+#target-input {
   margin-right: 5px;
 }
 
@@ -137,10 +112,6 @@ button {
   :root {
     color: #f6f6f6;
     background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
   }
 
   input,
