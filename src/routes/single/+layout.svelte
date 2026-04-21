bdb85fc1f20b6af1f7d719399fedb644fd9925f3
Peter Ling <44208962+ling1984@users.noreply.github.com>
2026-04-21 18:45:31 +0100

    Added some nice icons to layout on practice

--- src/routes/single/+layout.svelte
+++ src/routes/single/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { running, onStartDriver } from '$lib/stores';
   import { goto } from '$app/navigation';
+  import { ArrowLeft, Play, Pause } from '@lucide/svelte';
 
   async function handleButtonClick(event: Event) {
     const handler = $onStartDriver;
@@ -15,11 +16,15 @@
 
 <header class="header">
     <button class="btn" type="button" onclick={handleBackClick}>
-      Back
+      <ArrowLeft size={24}/>
     </button>
   <h1>Practice</h1>
     <button class="btn-drive {$running ? 'running' : 'stopped'}" type="button" onclick={handleButtonClick}>
-      {$running ? 'Stop' : 'Start'}
+      {#if $running}
+        <Pause size={24} />
+      {:else}
+        <Play size={24} />
+      {/if}
     </button>
 </header>
 
