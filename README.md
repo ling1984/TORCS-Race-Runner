fd0b3393fe6dfcf78ea3d839d6be1ca02feb3e91
Peter Ling <44208962+ling1984@users.noreply.github.com>
2026-04-28 15:38:28 +0100

    Added finished script for testing purposes. Updated README with banner info

--- README.md
+++ README.md
@@ -18,12 +18,21 @@ The map uses a mix of .png files and .rgb files from different directories, howe
 
 **Oil banner (3/4 row)** 64PASS1.png, 512x123, (0, 228)
 
+**Corkscrew Banner** 64PASS1.png, 492x71, (10, 133)
+
+**Small corkscrew banner** 64PASS1.png, 211x124, (216, 376)
+
+**Side corkscrew banner** 64PASS9.png, 211x119, (0,198)
+
 **Oli's Oil banner (1/3 row):** 64PASS6.png, 512x158, (0,0)
 
 [0] - this one is complicated because the wheels, parts of the lorry etc are on top of the logo.
 
 Body - 176x54, top: (0, 6) and (0, 67), bottom:(0, 134) and (0,195)
 Back - 36x47, top: (182, 10), bottom: (182, 138)
+
+Body-top is flipped on Y-axis.
+Back is rotated -90 degrees.
 
 ## Testing
 
--- testing/change_banners.py
+++ testing/change_banners.py
@@ -23,18 +23,34 @@ if banner_img_path:
     truck_coords = [[(0, 6), (0, 67), (0, 134), (0, 195)], [(182, 10), (182, 138)]]
 
     ## handle truck one in loop
-
+    loop=0
     for part, res, coords_list in zip(truck_parts, truck_res, truck_coords):
         base_path=os.path.join(corkscrew_path, truck_file)
         base = Image.open(base_path)
-
+        
         # prep banner img
         banner_img=Image.open(banner_img_path)
         banner_img.convert(base.mode)
-        banner_img_resized=banner_img.resize(res, Image.NEAREST)
 
+        # HACK very hacky. we flip 4 times so back to normal then rotate once.
         for pos in coords_list:
-            base.paste(banner_img_resized, pos, banner_img_resized)
+            if loop==0:
+                # we need to flip these on y-axis because 0,2 index are on the other side of the truck
+                banner_img = banner_img.transpose(Image.FLIP_LEFT_RIGHT)
+                # safe to resize here
+                banner_img=banner_img.resize(res, Image.NEAREST)
+
+            if loop==1:
+                banner_img = banner_img.transpose(Image.ROTATE_270)    
+                loop+=1
+                # we need to resize after rotating
+                banner_img=banner_img.resize(res, Image.NEAREST)
+            
+
+            # the ones at the back need to be rotated -90 degrees
+            base.paste(banner_img, pos, banner_img)
+            
+                
 
         # now put the outline on
         outline_path=os.path.join(corkscrew_path, truck_outline)
@@ -42,12 +58,13 @@ if banner_img_path:
         base.paste(outline_img, mask=outline_img)
 
         base.save(base_path)
+        loop+=1
 
     # handle rest of files
 
-    files = ["kilo.png", "TRUCK07.png", "64PASS1.png", "64PASS6.png"]
-    target_res = [(512, 256), (128, 64), (512, 123), (512, 158)]
-    start_coords = [(0, 0), (0, 64), (0, 228), (0, 0)]
+    files = ["kilo.png", "TRUCK07.png", "64PASS1.png", "64PASS1.png", "64PASS1.png", "64PASS6.png", "64PASS9.png"] # 3 locations in 64pass1.png
+    target_res = [(512, 256), (128, 64), (512, 123), (492, 71), (211, 124), (512, 158), (211, 119)]
+    start_coords = [(0, 0), (0, 64), (0, 228), (10, 133), (216, 376), (0, 0), (0,198)]
 
 
     for file, res, pos in zip(files, target_res, start_coords):
@@ -63,7 +80,7 @@ if banner_img_path:
         base.save(base_path)
 
 else:
-    files = ["kilo", "TRUCK07", "64PASS1", "64PASS6", "treeRNS2"]
+    files = ["kilo", "TRUCK07", "64PASS1", "64PASS6", "treeRNS2", "64PASS9"]
     for file in files:
         copy_path = os.path.join(corkscrew_path, file+"_copy.png")
         original_path = os.path.join(corkscrew_path, file+".png")
