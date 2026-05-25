493fc90bc80e7dfdc3df1348c8a903c54b1ac37a
Peter Ling <44208962+ling1984@users.noreply.github.com>
2026-05-25 17:12:58 +0100

    Added clean-up code, on closing the app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ use crate::{
 };
 use std::sync::Mutex;
 use tokio::sync::Mutex as TokioMutex;
+use tauri::{AppHandle, Manager, WindowEvent};
 
 mod car_logo;
 mod practice;
@@ -13,14 +14,40 @@ mod sgi_encoder;
 mod team_name;
 mod track_banners;
 
+async fn shutdown_sequence(app: AppHandle) {
+    let practice_state = app.state::<PracticeDriverState>();
+    let race_state = app.state::<RaceState>();
+
+    // normal sync function
+    let _ = stop_practice(practice_state);
+
+    // async function
+    let _ = stop_race(race_state).await;
+
+    // exit after async work completes
+    app.exit(0);
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
-        // .setup(|app| {
-        //     let store = app.store("settings.json")?;
-        //     Ok(())
-        // })
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            window.clone().on_window_event(move |event| {
+                if let WindowEvent::CloseRequested { api, .. } = event {
+                    println!("Window close requested");
+                    api.prevent_close();
+                    window.destroy().unwrap();
+                    let app_handle = window.app_handle().clone();
+                    tauri::async_runtime::spawn(async move {
+                        shutdown_sequence(app_handle).await;
+                    });
+                }
+            });
+
+            Ok(())
+        })
         .manage(PracticeDriverState {
             driver: Mutex::new(None),
             params: Mutex::new(None),
